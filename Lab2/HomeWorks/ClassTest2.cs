593cfbd564259820f21f5284c0b01552f48fc808
StrelovaEkaterina <strelovaekaterina99@gmail.com>
2021-11-25 17:43:21 +0300

    Lab2 complite

--- Lab2/HomeWorks/ClassTest2.cs
+++ Lab2/HomeWorks/ClassTest2.cs
@@ -1,10 +1,15 @@
 namespace Lab2.HomeWorks
 {
-    public class ClassTest2
+    public class ClassTest2 : AClassTest, Interface
     {
-        public string Print()
+        public override string Print()
         {
             return "It is ClassTest2";
+        }
+
+        public string PringFromInterface()
+        {
+            return "It is ClassTest2Interface";
         }
     }
 }
